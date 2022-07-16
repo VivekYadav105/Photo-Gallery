7a444b6c41970f5c3285217c01b2cae2a9b8ac96
vivek yadav <vivekyadav3133@gmail.com>
2022-07-17 00:19:27 +0530

    vercel-changes-2

--- server/routes/userroutes.js
+++ server/routes/userroutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const dotenv = require("dotenv");
 dotenv.config();
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 console.log(Number(process.env.NODE_TOKEN_TEMP_EXPIRE))
 
 const User = require("../models/usermodel");
